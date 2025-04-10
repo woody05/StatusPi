bcc5f4de1a4b78a93cee9ef993e32eb6a1cc7b50
woody05 <checkthisout58@gmail.com>
2025-04-10 14:00:16 -0400

    chanky attempt

--- app/rpi_ws281x_manager.py
+++ app/rpi_ws281x_manager.py
@@ -42,7 +42,9 @@ class RPIWS281XManager:
         self.strip.show()
 
     def set_brightness(self, brightness):
-        self.strip.setBrightness(brightness)
+        updated_strip = PixelStrip(self.strip.numPixels(), self.strip.pin, self.strip.freq, self.strip.dma,
+                                self.strip.invert, brightness, self.strip.channel)
+        self.strip = updated_strip
         self.strip.show()
 
     def set_color(self, color):
