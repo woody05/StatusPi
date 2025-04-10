234de4032924ac0e0b5c5213b6bf8fb9c41dd04a
woody05 <checkthisout58@gmail.com>
2025-04-10 10:10:41 -0400

    Updating to add rgb to settings.
    default led to white

--- app/rpi_ws281x_manager.py
+++ app/rpi_ws281x_manager.py
@@ -15,7 +15,7 @@ LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 50  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 25  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -32,11 +32,14 @@ class RPIWS281XManager:
         app.rpi_ws281x_manager = self
         self.debug = kwargs.get('debug', self.debug)
 
+        self.set_color('rgb(0, 0, 0)')  # Set initial color to black (off)
+        self.strip.show()
+
     def set_color(self, color):
 
         #TODO: find cleaner way to convert string color to Color object
         if isinstance(color, str):
-            color = color.strip("()").split(",")
+            color = color.strip("()").strip("rgb").split(",")
             red = int(color[0])
             green = int(color[1])
             blue = int(color[2])
