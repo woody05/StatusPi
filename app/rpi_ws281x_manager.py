3031202e54a1574b3ebd2e1956281f86c2073c66
woody05 <checkthisout58@gmail.com>
2025-04-20 22:28:42 -0400

    update light on start

--- app/rpi_ws281x_manager.py
+++ app/rpi_ws281x_manager.py
@@ -36,7 +36,7 @@ class RPIWS281XManager:
         self.debug = kwargs.get('debug', self.debug)
         self.settings_manager = app.settings_manager
 
-        self.set_color('rgb(255, 255, 255)')  # Set initial color to black (off)
+        # self.set_color('rgb(255, 255, 255)')  # Set initial color to black (off)
         self.set_brightness(self.settings_manager.get_settings().brightness)
         self.strip.show()
 
