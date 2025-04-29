b2b5e14754e1639b8ad7b44f1963bf9a4f8d18fa
woody05 <checkthisout58@gmail.com>
2025-04-29 08:07:53 -0400

    moving init logic

--- app/ina219_manager.py
+++ app/ina219_manager.py
@@ -64,10 +64,10 @@ class INA219Manager:
         self.addr = addr
 
         # Set chip to known config values to start
-        # self._cal_value = 0
-        # self._current_lsb = 0
-        # self._power_lsb = 0
-        # self.set_calibration_16V_5A()
+        self._cal_value = 0
+        self._current_lsb = 0
+        self._power_lsb = 0
+        self.set_calibration_16V_5A()
 
     def init_app(self, app, i2c_bus=1, addr=0x40, **kwargs):
         app.ina219_manager = self
@@ -77,11 +77,11 @@ class INA219Manager:
         self.bus = smbus.SMBus(i2c_bus);
         self.addr = addr
 
-        # Set chip to known config values to start
-        self._cal_value = 0
-        self._current_lsb = 0
-        self._power_lsb = 0
-        self.set_calibration_16V_5A()
+        # # Set chip to known config values to start
+        # self._cal_value = 0
+        # self._current_lsb = 0
+        # self._power_lsb = 0
+        # self.set_calibration_16V_5A()
 
     def read(self,address):
         data = self.bus.read_i2c_block_data(self.addr, address, 2)
